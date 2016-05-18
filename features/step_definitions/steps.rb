ed80b03ec24d2f8de60a9b91711f1c4b2768ba37
Raj B <rags4developer@gmail.com>
2016-05-17 22:21:27 -0700

    Satisfied the 'When' requirement bysearching for a word in youtube.

--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -2,8 +2,9 @@ Given(/^I am on the Youtube home page$/) do
   visit 'http://www.youtube.com'
 end
 
-When(/^I search for "([^"]*)"$/) do |arg1|
-  pending # Write code here that turns the phrase above into concrete actions
+When(/^I search for "([^"]*)"$/) do |search_term|
+  fill_in 'search_query', :with => search_term
+  click_on 'search-btn'
 end
 
 Then(/^videos of large rodents are returned$/) do
