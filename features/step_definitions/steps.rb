6c160af750e68d9828e2f52e51d9c3ea2bb17e53
Raj B <rags4developer@gmail.com>
2016-05-17 18:03:09 -0700

    created steps.rb & put template ruby code which will be used to implement the steps for the only scenario in the feature file. Notes- run 'bundle exec cucumber'; copy the Given, When, Then templates in the output & paste into steps.rb

---
+++ features/step_definitions/steps.rb
@@ -0,0 +1,12 @@
+Given(/^I am on the Youtube home page$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I search for "([^"]*)"$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^videos of large rodents are returned$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
